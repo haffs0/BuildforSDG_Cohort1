681d51bc6957550dd1018bd8ae3d1007f44788c1
haffs <abuafeezabolarinwa@yahoo.com>
2020-04-09 09:35:09 -0700

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   rules: {
     "indent": ['error', 2],
+    "eol-last": ["error", "never"],
     "comma-dangle": ["error", "never"],
     "no-param-reassign": ["error", {
       "props": false
--- src/estimator.js
+++ src/estimator.js
@@ -9,7 +9,7 @@ const convertWeeksAndMonthsToDays = (periodType, timeToElapse) => {
     days: estimateTime,
     power: Math.floor(estimateTime / 3)
   };
-}
+};
 const covid19ImpactEstimator = (data) => {
   const {
     region,
@@ -18,6 +18,10 @@ const covid19ImpactEstimator = (data) => {
     timeToElapse,
     totalHospitalBeds
   } = data;
+  const {
+    avgDailyIncomeInUSD, 
+    avgDailyIncomePopulation 
+  } = region;
   const impact = {};
   const severeImpact = {};
   const {
@@ -31,22 +35,23 @@ const covid19ImpactEstimator = (data) => {
   impact.severeCasesByRequestedTime = 0.15 * impact.infectionsByRequestedTime;
   severeImpact.severeCasesByRequestedTime = 0.15 * severeImpact.infectionsByRequestedTime;
   impact.hospitalBedsByRequestedTime = totalHospitalBeds - impact.severeCasesByRequestedTime;
-  severeImpact.hospitalBedsByRequestedTime = totalHospitalBeds - severeImpact.severeCasesByRequestedTime;
+  const difference = totalHospitalBeds - severeImpact.severeCasesByRequestedTime;
+  severeImpact.hospitalBedsByRequestedTime = difference;
   const impactInfections = Math.floor(impact.infectionsByRequestedTime);
   const severeInfections = Math.floor(severeImpact.infectionsByRequestedTime);
   impact.casesForICUByRequestedTime = 0.05 * impactInfections;
   severeImpact.casesForICUByRequestedTime = 0.05 * severeInfections;
   impact.casesForVentilatorsByRequestedTime = 0.02 * impactInfections;
   severeImpact.casesForVentilatorsByRequestedTime = 0.02 * severeInfections;
-  const amountImpact = impactInfections * region.avgDailyIncomePopulation * region.avgDailyIncomeInUSD * days;
+  const amountImpact = impactInfections * avgDailyIncomePopulation * avgDailyIncomeInUSD * days;
   impact.dollarsInFlight = amountImpact.toFixed(2);
-  const amountInFlight = severeInfections * region.avgDailyIncomePopulation * region.avgDailyIncomeInUSD * days;
+  const amountInFlight = severeInfections * avgDailyIncomePopulation * avgDailyIncomeInUSD * days;
   severeImpact.dollarsInFlight = amountInFlight.toFixed(2);
-  return {
+  const result = {
     ...data,
     impact,
     severeImpact
   };
+  return result;
 };
-
 export default covid19ImpactEstimator;
